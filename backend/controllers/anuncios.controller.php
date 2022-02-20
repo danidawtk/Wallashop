4879f68f8a37e6c2252b883e358f7cef557b4431
danidawtk <75862411+danidawtk@users.noreply.github.com>
2022-02-20 15:40:14 +0100

    Add files via upload

--- backend/controllers/anuncios.controller.php
+++ backend/controllers/anuncios.controller.php
@@ -124,5 +124,56 @@ class AnunciosController {
       exit(json_encode(["error" => "Fallo de autorizacion"]));            
     }
   }
+    
+    public function subirAvatarAnu() {
+    if(is_null(IDUSER)){
+      http_response_code(401);
+      exit(json_encode(["error" => "Fallo de autorizacion"]));
+    }
+    if(isset($_FILES['imagenanu'])) {
+      $imagenanu = $_FILES['imagenanu'];
+      $mime = $imagenanu['type'];
+      $size = $imagen['size'];
+      $rutaTemp = $imagenanu['tmp_name'];
+  
+      //Comprobamos que la imagen sea JPEG o PNG y que el tamaño sea menor que 400KB.
+      if( !(strpos($mime, "jpeg") || strpos($mime, "png")) || ($size > 400000) ) {
+        http_response_code(400);
+        exit(json_encode(["error" => "La imagen tiene que ser JPG o PNG y no puede ocupar mas de 400KB"]));
+      } else {
+  
+        //Comprueba cual es la extensión del archivo.
+        $ext = strpos($mime, "jpeg") ? ".jpg":".png";
+        $nombreFoto = "p-".IDUSER."-".time().$ext;
+        $ruta = ROOT."imagesanu/".$nombreFoto;
+  
+        //Comprobamos que el usuario no tenga mas fotos de perfil subidas al servidor.
+        //En caso de que exista una imagen anterior la elimina.
+        $imgFind = ROOT."imagesanu/p-".IDUSER."-*";
+        $imgFile = glob($imgFind);
+        foreach($imgFile as $fichero) unlink($fichero);
+        
+        //Si se guarda la imagen correctamente actualiza la ruta en la tabla usuarios
+        if(move_uploaded_file($rutaTemp,$ruta)) {
+  
+          //Prepara el contenido del campo imgSrc
+          $imgSRC = "http://localhost/EL_TRABAJAZO/".basename(ROOT)."/imagesanu/".$nombreFoto;
+  
+          $eval = "UPDATE anuncio SET fotoanu=? WHERE idanu=?";
+          $peticion = $this->db->prepare($eval);
+          $peticion->execute([$anuncio->fotoanu,$anuncio->idanu]);
+  
+          http_response_code(201);
+          exit(json_encode("Imagen actualizada correctamente"));
+        } else {
+          http_response_code(500);
+          exit(json_encode(["error" => "Ha habido un error con la subida"]));      
+        }
+      }
+    }  else {
+      http_response_code(400);
+      exit(json_encode(["error" => "No se han enviado todos los parametros"]));
+    }
+  }
 }
     

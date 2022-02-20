c61ed70442511083d71ec7b3371185e0e816158b
danidawtk <75862411+danidawtk@users.noreply.github.com>
2022-02-20 15:38:46 +0100

    Add files via upload

--- backend/proyfin.sql
+++ backend/proyfin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-02-2022 a las 13:41:47
+-- Tiempo de generación: 19-02-2022 a las 15:25:28
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.8
 
@@ -51,11 +51,11 @@ INSERT INTO `anuncio` (`idanu`, `titulo`, `texto`, `precio`, `fotoanu`, `fecha`,
 (38, 'Se alquila piso', 'En madrid ', '10000000000000', 'http://localhost/EL_TRABAJAZO/backend/images/p-9-1640703565.jpg', '2021-08-16 09:13:44', 'Decoracion', 13),
 (46, 'Vendo ordenador', 'Componentes:\n-i3\n-8gb ram', '2000', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2021-12-13 09:44:35', 'Electronica', 13),
 (54, 'foto3', '1', '1', 'http://localhost/EL_TRABAJAZO/backend/images/p-4-1615701833.jpg', '2022-01-10 19:26:24', 'Otros', 3),
-(69, 'Fotaza', 'fotaza', '10', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-01-11 13:00:49', 'Otros', 4),
-(79, 'Vendo cajas de cartón', 'Cajas acartonadas, color cartón, con un ligero aroma a cartón y con un sabor y textura propias de los mejores cartones de la Península Ibérica.', 'Unidad/0,3', 'http://localhost/EL_TRABAJAZO/backend/images/cartonoso.jpg', '2022-01-14 17:09:18', 'Decoracion', 5),
-(80, 'Vendo Xbox', 'No se enciendo, pero es bonita.', '30,43', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-01-25 16:27:28', 'Electronica', 5),
+(69, 'Fotaza', 'fotaza', '1000', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-01-11 13:00:49', 'Otros', 4),
 (82, 'Vendo alimento', 'si', '1', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-02-01 15:38:27', 'Alimentacion', 3),
-(83, 'Vendo móviles ', 'Móviles con un dibujo de una pera por detrás.', '300', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-02-04 15:46:29', 'Electronica', 4);
+(83, 'Vendo móviles ', 'Móviles con un dibujo de una pera por detrás.', '300', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-02-04 15:46:29', 'Electronica', 4),
+(86, 'Vendo azuzar', 'Azucar azucarado', '32', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-02-15 17:14:55', 'Alimentacion', 4),
+(87, 'Vendo radiador', 'dsads', '10', 'http://localhost/EL_TRABAJAZO/backend/images/SIN-IMAGEN.jpg', '2022-02-16 17:23:48', 'Decoracion', 4);
 
 -- --------------------------------------------------------
 
@@ -81,8 +81,7 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `password`, `poblacion`, `provincia`, `email`, `telefono`, `foto`) VALUES
 (3, 'Ivan', 'a', '$2y$10$GcxE/fRj1ZHFerQZNSd6W.CyqYH.Zl3GGf6Yfi4tN2zCdO4KD0N5i', 'Alc', 'Ciud', 'iva@ho', '31221312', 'http://localhost/proyfin/images/p-3-1615692525.jpg'),
-(4, 'Daniel', 'abe', '$2y$10$NZLtyuGB9clyrIe3SbEdRuJGmr5KUqUf1kNtWWJncVg3YbTdh82Aq', 'Alc', 'Ciud', 'a@aa', '31221312', 'http://localhost/EL_TRABAJAZO/backend/images/p-4-1642177431.jpg'),
-(5, 'Jorge', 'leal', '$2y$10$TLdm8apj0DSNi7wpavo/p.vKnyJEUam8zpFxgJZRC/fTeyzgOCOtO', 'crip', 'Ciud', 'b@bb', '123', 'http://localhost/proyfin/images/p-5-1626109786.jpg'),
+(4, 'Daniel', 'abeng', '$2y$10$NZLtyuGB9clyrIe3SbEdRuJGmr5KUqUf1kNtWWJncVg3YbTdh82Aq', 'Alc', 'Ciud', 'a@aa', '31221312', 'http://localhost/EL_TRABAJAZO/backend/images/p-4-1642177431.jpg'),
 (8, '3', 'e', '$2y$10$the9Vt/KcYBUxXYId2gdKOlfRStYgTn.XvBd7m/neCUn6ma8oYnR6', 'e', 'e', 'e@ee', '31221312', NULL),
 (9, '1', '2', '$2y$10$l0zV/S9kyHwRJM8edkIZsu1eWkTqH8O.V1HlcBlHBLKPG6yhAauYO', 'ewq', 'ewq', 'a@aaa', '31221312', 'http://localhost/proyfin/images/p-9-1640703565.jpg'),
 (10, '¨sd&%', 'ewq', '$2y$10$SywU/Dbc8PO56z1PIgqAb.9TJNc1UW4M1CufCRN9.CUiu.HWtjhQq', 'e', 'e', 'e@asd', '11', NULL),
@@ -114,13 +113,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `anuncio`
 --
 ALTER TABLE `anuncio`
-  MODIFY `idanu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `idanu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
@@ -130,7 +129,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `anuncio`
 --
 ALTER TABLE `anuncio`
-  ADD CONSTRAINT `anuncio_ibfk_1` FOREIGN KEY (`idanunciante`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `anuncio_ibfk_1` FOREIGN KEY (`idanunciante`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
